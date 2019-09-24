521a2a8f72019972ecadf384f1ee76d23ed93bf0
armi3 <fernandagonzalez@ufm.edu>
2019-09-23 21:25:31 -0600

    Portal function.

--- soup.py
+++ soup.py
@@ -2,6 +2,7 @@ import json
 import sys
 from bs4 import BeautifulSoup
 import requests
+import re
 
 
 def soup_html_content(url):
@@ -18,9 +19,26 @@ def soup_html_content(url):
 
 def portal():
     souped_html_content = soup_html_content("http://ufm.edu/Portal")
-    print("\nTitle: ", souped_html_content.title.string)
-    # print("\nAddress: ", souped_html_content.title.string)
-    pass
+    footer = souped_html_content.find(id="footer")
+    footer_2 = footer.find_all('div', class_='span4')
+    print("\nTitle:     ", souped_html_content.title.string)
+    print("Address:   ", footer_2[0].a.text)
+    print("Phone:     ", footer_2[1].a.text)
+    print("Email:     ", footer_2[1].findAll('a')[1].text)
+    print("\nUpper nav menu items:")
+    for item in souped_html_content.find(id="menu-table").find_all(class_="menu-key"):
+        print(" ▫️ ", item.text.strip())
+    print("\nProperties that have href's:")
+    for link in souped_html_content.find_all('a', attrs={'href': re.compile("^(https)|(http)")}):
+        print(" ▫️ ", link.get('href'))
+    print("\nHref of 'UFMail' button: ", souped_html_content.find(id='ufmail_')['href'])
+    print("\nHref of 'MiU' button: ", souped_html_content.find(id='miu_')['href'])
+    print("\nHref of all <img>:")
+    img_count = 0
+    for img in souped_html_content.find_all('img', attrs={'src': re.compile("^(https)|(http)|(/)")}):
+        print(" ▫️ ", img.get('src'))
+        img_count += 1
+    print("\nCount of all <img>: ", img_count)
 
 
 def studies():
@@ -51,8 +69,5 @@ elif sys.argv[1] == 4:
     directory()
 
 
-soup_test = soup_html_content("http://ufm.edu/Portal")
-#print(soup_test.prettify())
-footer = soup_test.find(id="footer")
-footer2 = footer.find_all('div', class_='span4')
-print(footer2)
+# soup_test = soup_html_content("http://ufm.edu/Portal")
+portal()
